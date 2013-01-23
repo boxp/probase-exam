f6a57bd54cfbcd32c15717c9fffd83f6597bab6c
boxp <tiyotiyouda@gmail.com>
2013-01-23 18:59:56 +0900

    22問目までオワター

---
+++ prokiso_answer/12-big&small.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main(int argc, char const* argv[])
+{
+	int big = 15;
+	int small = -8;
+
+	printf("big = %d\nsmall = %d\n", big,small);
+	printf("sum = %d\n", big+small);
+	printf("dif = %d\n", big-small);
+	printf("pro = %d\n", big*small);
+
+	return 0;
+}
